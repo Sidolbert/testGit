056717ac4f799f42406547a0b19e8c0ee2094063
c.enrici <c.enrici@PARNOT.pedago.ccinca.fr>
2022-06-13 09:54:29 +0200

    premier commit
    programme affichant une ligne de texte

---
+++ src/testGit/Main.java
@@ -0,0 +1,13 @@
+package testGit;
+
+public class Main {
+
+	
+	//projet de test des fonctionnalités de Git avec Eclipse et GitHub
+	public static void main(String[] args) {
+		
+		System.out.println("mon projet fonctionne !");
+
+	}
+
+}
